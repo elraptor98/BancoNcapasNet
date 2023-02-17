4e95b27ce4e4733b18ac7ddc2808c08fec3d88ee
unknown <peru2020dot@gmail.com>
2023-02-17 12:18:13 -0500

    se agrego las entidades cliente, TipoCliente, TipoDocumento

---
+++ Banco.Entidades/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco.Entidades
+{
+    public class Cliente
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+
+        public int Apellidos { get; set; }
+
+        public string RazonSocial { get; set; }
+
+        public int NumeroDocumento { get; set; }
+
+        public int IdTipoCliente { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
